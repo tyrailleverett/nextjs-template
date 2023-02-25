84ce9451fb337b90a58a377007be6a6f611f6d79
Tyrail Leverett <tyrailleverett@gmail.com>
2023-02-24 23:30:32 -0600

    Added eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { MantineProvider } from '@mantine/core';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-      >
-      <Component {...pageProps} /></MantineProvider>)
-
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <Component {...pageProps} />
+  </MantineProvider>
+);
+export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,21 @@
-import { createGetInitialProps } from '@mantine/next';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 const getInitialProps = createGetInitialProps();
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export default class _Document extends Document {
-    static getInitialProps = getInitialProps;
+  static getInitialProps = getInitialProps;
 
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
